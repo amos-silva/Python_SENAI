c4ef56a6bf2c61496bd2a659309568798c2db868
amos-silva <amosdasilva86@gmail.com>
2025-06-06 19:43:45 -0300

    Add files via upload

---
+++ Python Developer QUA.491.006/01_logica_de_programacao/03_variaveis/main.py
@@ -0,0 +1,33 @@
+# comentario de uma linha
+
+"""
+comentario de varias linhas
+"""
+
+#TODO - trecho para fazer
+#NOTE - nota sobre o trecho de codigo
+#FIXME - corrigir o trecho de codigo
+#REVIEW - revisar trecho de codigo
+#ANCHOR - marcar trecho de codigo
+#SECTION - iniciar uma seção em um trecho de codigo
+#!SECTION - termina uma seção em um trecho de codigo
+
+# Declaração de variáveis
+texto = 'Este é um texto ou String'
+numero_inteiro = 25
+numero_decimal = 7.5
+programador_verdade = True
+programador_falso = False
+
+# saida de dados
+print(texto)
+print(numero_inteiro)
+print(numero_decimal)
+print(programador_verdade)
+print(programador_falso)
+
+print(type(texto))
+print(type(numero_inteiro))
+print(type(numero_decimal))
+print(type(programador_verdade))
+print(type(programador_falso))
